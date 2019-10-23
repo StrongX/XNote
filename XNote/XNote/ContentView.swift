929a6ff90b0acaaef62bf059b08209a861947bfa
StrongX <strongxlx@gmail.com>
2019-10-23 21:05:48 +0800

    Install

---
+++ XNote/XNote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XNote
+//
+//  Created by 许莉鑫 on 2019/10/23.
+//  Copyright © 2019 许莉鑫. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
